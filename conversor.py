b711dc10c98550edc0fececba93944b4097cd9ff
Ederson Torresini <boidacarapreta@gmail.com>
2019-02-20 12:45:21 -0300

    Converter de binário para decimal

--- conversor.py
+++ conversor.py
@@ -2,7 +2,6 @@
 decimal = 80
 print('Número decimal: ' + str(decimal))
 
-
 # A string do número binário começa vazia
 binario = ''
 
@@ -72,3 +71,42 @@ else:
 
 # Apresentação do resultado final
 print('Convertido para binário: ' + binario)
+
+# Converter de binário para decimal
+decimal = 0
+
+# Cálculo do primeiro dígito: 2^7
+# Se a posição 0 (zero) do número binário for 1 (um), adiciona 128 (2^7)
+if binario[0] == '1':
+    decimal = decimal + 128
+
+# Cálculo do segundo dígito: 2^6
+if binario[1] == '1':
+    decimal = decimal + 64
+
+# Cálculo do terceiro dígito: 2^5
+if binario[2] == '1':
+    decimal = decimal + 32
+
+# Cálculo do quarto dígito: 2^4
+if binario[3] == '1':
+    decimal = decimal + 16
+
+# Cálculo do quinto dígito: 2^3
+if binario[4] == '1':
+    decimal = decimal + 8
+
+# Cálculo do sexto dígito: 2^2
+if binario[5] == '1':
+    decimal = decimal + 4
+
+# Cálculo do sétimo dígito: 2^1
+if binario[6] == '1':
+    decimal = decimal + 2
+
+# Cálculo do oitavo dígito: 2^0
+if binario[7] == '1':
+    decimal = decimal + 1
+
+# Apresentação da prova real
+print('Convertido de volta para decimal: ' + str(decimal))
