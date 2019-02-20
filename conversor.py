5fbbc083a2b266828accb7c48c1c519bf7cbe314
Ederson Torresini <boidacarapreta@gmail.com>
2019-02-20 13:13:42 -0300

    Usar estrutura de repetição

--- conversor.py
+++ conversor.py
@@ -17,69 +17,18 @@ print('Número decimal: ' + str(decimal))
 # A string do número binário começa vazia
 binario = ''
 
-# Cálculo do primeiro dígito: 2^7
-doisnasete = decimal - 128
-if doisnasete >= 0:
-    binario = binario + '1'
-    decimal = decimal - 128
-else:
-    binario = binario + '0'
-
-# Cálculo do segundo dígito: 2^6
-doisnaseis = decimal - 64
-if doisnaseis >= 0:
-    binario = binario + '1'
-    decimal = decimal - 64
-else:
-    binario = binario + '0'
-
-# Cálculo do terceiro dígito: 2^5
-doisnacinco = decimal - 32
-if doisnacinco >= 0:
-    binario = binario + '1'
-    decimal = decimal - 32
-else:
-    binario = binario + '0'
-
-# Cálculo do quarto dígito: 2^4
-doisnaquatro = decimal - 16
-if doisnaquatro >= 0:
-    binario = binario + '1'
-    decimal = decimal - 16
-else:
-    binario = binario + '0'
-
-# Cálculo do quinto dígito: 2^3
-doisnatres = decimal - 8
-if doisnatres >= 0:
-    binario = binario + '1'
-    decimal = decimal - 8
-else:
-    binario = binario + '0'
-
-# Cálculo do sexto dígito: 2^2
-doisnadois = decimal - 4
-if doisnadois >= 0:
-    binario = binario + '1'
-    decimal = decimal - 4
-else:
-    binario = binario + '0'
-
-# Cálculo do sétimo dígito: 2^1
-doisnaum = decimal - 2
-if doisnaum >= 0:
-    binario = binario + '1'
-    decimal = decimal - 2
-else:
-    binario = binario + '0'
-
-# Cálculo do oitavo dígito: 2^0
-doisnazero = decimal - 1
-if doisnazero >= 0:
-    binario = binario + '1'
-    decimal = decimal - 1
-else:
-    binario = binario + '0'
+# Usando laço de repetição para os oito bits
+# A função range cria uma lista de 8 elementos,
+# sendo 7 (sete) o primeiro (limite incluído)
+# e 0 (zero) o último (limite não incluído)
+# Detalhe para o passo (step) negativo para ordem descrescente
+for digito in range(7, -1, -1):
+    resto = decimal - pow(2, digito)
+    if resto >= 0:
+        binario = binario + '1'
+        decimal = decimal - pow(2, digito)
+    else:
+        binario = binario + '0'
 
 # Apresentação do resultado final
 print('Convertido para binário: ' + binario)
@@ -87,38 +36,13 @@ print('Convertido para binário: ' + binario)
 # Converter de binário para decimal
 decimal = 0
 
-# Cálculo do primeiro dígito: 2^7
-# Se a posição 0 (zero) do número binário for 1 (um), adiciona 128 (2^7)
-if binario[0] == '1':
-    decimal = decimal + 128
-
-# Cálculo do segundo dígito: 2^6
-if binario[1] == '1':
-    decimal = decimal + 64
-
-# Cálculo do terceiro dígito: 2^5
-if binario[2] == '1':
-    decimal = decimal + 32
-
-# Cálculo do quarto dígito: 2^4
-if binario[3] == '1':
-    decimal = decimal + 16
-
-# Cálculo do quinto dígito: 2^3
-if binario[4] == '1':
-    decimal = decimal + 8
-
-# Cálculo do sexto dígito: 2^2
-if binario[5] == '1':
-    decimal = decimal + 4
-
-# Cálculo do sétimo dígito: 2^1
-if binario[6] == '1':
-    decimal = decimal + 2
-
-# Cálculo do oitavo dígito: 2^0
-if binario[7] == '1':
-    decimal = decimal + 1
+# Usando laço de repetição para fazer a prova real
+# de binário para decimal
+# Detalhe para a sequência de dígitos que é inversa à potência de 2 (dois)
+# por isso o uso de valor absoluto (módulo) do número
+for digito in range(0, 8, 1):
+    if binario[digito] == '1':
+        decimal = decimal + pow(2, abs(digito - 7))
 
 # Apresentação da prova real
 print('Convertido de volta para decimal: ' + str(decimal))
